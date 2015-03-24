b69a32b6935fd124a52212b745633ffa1f623bb6
gauravjetley <gauravjetley@gmail.com>
2015-03-24 15:11:43 -0400

    Adding files

--- colummean.R
+++ colummean.R
@@ -1,11 +1,11 @@
 colummean <- function(x, removeNA = TRUE) {
-  
-  #This function takes gets the means of colums in a dataset
-  
-  numcolums <- ncol(x)
-  meancolums <- numeric(ncol(x))
-      for(i in 1:numcolums) {
+    
+#This function takes gets the means of colums in a dataset
+    
+    numcolums <- ncol(x)
+    meancolums <- numeric(ncol(x))
+    for(i in 1:numcolums) {
         meancolums[i] <- mean(x[,i], na.rm = removeNA)
-      }
-  meancolums
+    }
+    meancolums
 }
