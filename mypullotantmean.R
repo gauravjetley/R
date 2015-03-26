387b3aec4e0554d1dd666a234679b5ed29880fd3
gauravjetley <gauravjetley@gmail.com>
2015-03-25 21:52:12 -0400

    working on single csv
    Mean is incorrect for multiple .csv's

--- mypullotantmean.R
+++ mypullotantmean.R
@@ -1,21 +1,30 @@
 pollutantmean <- function (directory, pollutant, id = 1:332) {
-    #setwd(dir = paste(directory,sep="")
-    #meandata <- matrix()
+    
+    #getting and setting wd
+    currentwd<-getwd()
+    if (grep(pattern = directory,x = currentwd)) {}
+    else {setwd(paste("./",directory,"/",sep=""))}
+    
+    #declaring objects
     x<-1    
+    poll<-pollutant
+    meandata<-c()
+    
     #Looping through the files
     for (i in id){    
         
         #Appending leading zero's to the file names
-        j<-i
-        if (j<10) {j<-paste("00",j,".csv",sep="")}
-        else if (j>=10 & j<100) {j<-paste("0",j,".csv",sep="")}
-        else {j<-j}
+        j<-c()
+        if (i<10) {j<-paste("00",i,".csv",sep="")}
+        else if (i>=10 & i<100) {j<-paste("0",i,".csv",sep="")}
+        else {j<-paste(i,".csv",sep="")}
         
-        #Adding means of files in temp
+        #Adding means of pollutant in meandata
         temp <- read.csv(file = j, head = TRUE, sep = ",")
-        meandata[x] <- mean(temp$nitrate, na.rm = TRUE)
+        meandata[x] <- mean(temp[,poll], na.rm = TRUE)
         x<-x+1
     }
     
-    mean(meandata)
+    #rounding and displaying values
+    round(mean(meandata),3)
 }
